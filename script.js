42629675c1dfabfe8862c902008e87056b6d6561
SoundDobad <SoundDobad@gmail.com>
2025-04-28 23:18:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    setTimeout(() => {
+        const container = document.querySelector('.logo-container');
+        const wrapper = document.querySelector('.logo-wrapper');
+
+        if (window.innerWidth <= 600) {
+    container.style.top = "13%"; /* <<< higher up for mobile */
+    wrapper.style.width = "150px";
+} else {
+    container.style.top = "13%";
+    wrapper.style.width = "200px";
+}
+
+        setTimeout(() => {
+            const links = document.querySelector('.links');
+            links.style.visibility = "visible";
+            links.classList.add('show');
+
+            document.body.style.overflow = "auto";
+
+            // No scroll event! No fade-out needed.
+
+        }, 500); 
+
+    }, 2000); 
+};
